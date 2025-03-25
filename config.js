5d77de30f6990bff512dfbed0b1e8e830c74e19c
BrayanOFC <lourdesagueda93@gmail.com>
2025-03-25 14:54:08 -0700

    Actualizar config.js

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ global.owner = [
   ['595976126756', '👑 Miku - AdrianOficial 👑', true],
   ['595994836199', '💫 Miku - Collaborator 2 💫', true],
   ['584245610338', '💫 Miku - Collaborator 3 💫', true],
+  ['5216633900512', '💫 Miku - Collaborator 4💫', true],
   ['595994825505']
 ];
 
